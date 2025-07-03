eaa6aa1e0e1a6b1fc35aeefdeebbc292a10b94b8
CrabbyComrade <184875117+CrabbyComrade@users.noreply.github.com>
2025-07-04 01:54:29 +0200

    added main() to zoo class

--- src/main/java/zoo/Zoo.java
+++ src/main/java/zoo/Zoo.java
@@ -3,14 +3,17 @@ package zoo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Zoo<H extends Habitat<A>, A extends Animal> {
-    private List<H> habitats;
+import zoo.fishes.Catfish;
+import zoo.mammals.cats.Tiger;
+
+public class Zoo {
+    private List<Habitat<? extends Animal>> habitats;
 
     public Zoo() {
         habitats = new ArrayList<>();
     }
 
-    public void build(H habitat) { // Adds a habitat to zoo
+    public void build(Habitat<? extends Animal> habitat) { // Adds a habitat to zoo
         if (!habitats.contains(habitat)) { // Only add if its not already there
             habitats.add(habitat);
             System.out.println(habitat.getName() + " has been built in zoo.");
@@ -19,7 +22,7 @@ public class Zoo<H extends Habitat<A>, A extends Animal> {
         }
     }
 
-    public void abandon(H habitat) { // Removes a habitat from zoo
+    public void abandon(Habitat<? extends Animal> habitat) { // Removes a habitat from zoo
         if (habitats.contains(habitat)) { // Only try to remove if there is a habitat to remove
             habitats.remove(habitat);
             System.out.println(habitat.getName() + " has been abandoned from zoo.");
@@ -30,8 +33,21 @@ public class Zoo<H extends Habitat<A>, A extends Animal> {
 
     public void visitAllHabitats() { // Prints names of all habitats
         System.out.println("Visiting habitats in  zoo:");
-        for (H hab : habitats) {
+        for (Habitat<? extends Animal> hab : habitats) {
             System.out.println(hab.getName());
         }
+    }
+
+    public static void main(String[] args) {
+        // habitats accepting one animal type each
+        Habitat<Tiger> tigerHabitat = new Habitat<>("Tiger Habitat");
+        Habitat<Catfish> catfishHabitat = new Habitat<>("Catfish Habitat");
+
+        Zoo zoo = new Zoo();
+
+        zoo.build(tigerHabitat);
+        zoo.build(catfishHabitat);
+
+        zoo.visitAllHabitats();
     }
 }
